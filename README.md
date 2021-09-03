1b7d9de3eb95a0a458be76d3b4b83d085ef22e6d
duypv <viet.duy.pham@gmail.com>
2021-09-03 12:45:37 +0700

    list, add, update, delete book

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ vi ~/.zshrc
 Paste export PATH=$HOME/.dotnet/tools:$PATH
 Run the command :wq to write the file to disk and quit
 Restart Terminal.app and you should be able to run abp --help
+
+## DB
+
+Enter admin as the username and 1q2w3E* as the password to login to the application. The application is up and running. You can start developing your application based on this startup template.
--- src/aspnet-core/src/Acme.BookStore.MongoDB/MongoDb/BookStoreMongoDbContext.cs
+++ src/aspnet-core/src/Acme.BookStore.MongoDB/MongoDb/BookStoreMongoDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Volo.Abp.Data;
+﻿using Acme.BookStore.Books;
+using MongoDB.Driver;
+using Volo.Abp.Data;
 using Volo.Abp.MongoDB;
 
 namespace Acme.BookStore.MongoDB
@@ -9,6 +11,7 @@ namespace Acme.BookStore.MongoDB
         /* Add mongo collections here. Example:
          * public IMongoCollection<Question> Questions => Collection<Question>();
          */
+        public IMongoCollection<Book> Books => Collection<Book>();
 
         protected override void CreateModel(IMongoModelBuilder modelBuilder)
         {
